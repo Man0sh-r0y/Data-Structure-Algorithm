0cb88b9063c6c942d8f3319f19f646fbda4bfa0c
Manash <manashroy454@gmail.com>
2023-03-06 22:33:26 +0530

    Edited files

--- Binary Tree/VerticalOrderByLevelOrder.java
+++ Binary Tree/VerticalOrderByLevelOrder.java
@@ -1,8 +1,9 @@
+import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.TreeMap;
 import java.util.Queue;
 
-public class BottomViewOfBinaryTree {
+public class VerticalOrderByLevelOrder {
 
     static class Node {
         int data;
@@ -26,25 +27,30 @@ public class BottomViewOfBinaryTree {
         }
     }
 
-    // I'll use level order traverse to get the Bottom View
-    public static void bottomView(Node root) {
+    // printing vertical order By traversing in level order
+    public static void verticalOrder(Node root) {
         // I want to map each Node with its vertical index (Column index)
-        TreeMap<Integer, Integer> map = new TreeMap<>();
+        TreeMap<Integer, ArrayList<Integer>> map = new TreeMap<>();
         // Difference between TreeMap and HashMap in Java is Mostly Tree map stores the
         // data in sorted order key wise where the HashMap doesn't.
         // here we needed the key in Sorted order so we used TreeMap.
-        // Here, Key is the Column index and value is last Node at each column
-        // we have to take each columns's last Node
+        // In this TreeMap, Key is the Column index and value is Nodes at each column
+        // Here in each key, it might have multiple values because:
+        // in the each column there might have multiple Nodes So we took ArrayList
         Queue<Pair> q = new LinkedList<>();
-        q.add(new Pair(root, 0));
+        q.add(new Pair(root, 0));// root with its column index = 0
+        // now we will do level order traversal
         while (!q.isEmpty()) {
-            Pair currPair = q.remove();
-            Node currNode = currPair.node;
-            int colIndex = currPair.key;
-            map.put(colIndex, currNode.data);
-            // if any key (column index) present in TreeMap then its value will be replaced
-            // with the new key Value with remaining key in TreeMap
-            // As I want last node of each column. That's bottom view
+            Pair currPair = q.remove(); // getting out the current Pair
+            Node currNode = currPair.node; // current Node
+            int colIndex = currPair.key; // column indx or vertical line number of current node
+
+            if (!map.containsKey(colIndex)) {
+                // containsKey() method checks:
+                // if the mapping for the specified key is present in the hashmap.
+                map.put(colIndex, new ArrayList<>());
+            }
+            map.get(colIndex).add(currNode.data);// adding node in the map according to its column index
             if (currNode.left != null) {
                 q.add(new Pair(currNode.left, colIndex - 1));// traversing in left decrease the colIndex
             }
@@ -53,7 +59,8 @@ public class BottomViewOfBinaryTree {
             }
         }
 
-        System.out.println(map.values());// printing the bottom view nodes
+        System.out.println(map.values());// printing the Vertical Order Traversal
+        // values will be printed according to the sorted order of column Index
     }
 
     public static void main(String[] args) {
@@ -64,9 +71,7 @@ public class BottomViewOfBinaryTree {
         root.left.right = new Node(5);
         root.right.left = new Node(6);
         root.right.right = new Node(7);
-        root.left.right.left = new Node(8);
-        root.left.right.right = new Node(9);
 
-        bottomView(root);
+        verticalOrder(root);
     }
 }
--- Binary Tree/VerticalOrderTraversal.java
+++
@@ -1,5 +0,0 @@
-public class VerticalOrderTraversal {
-    public static void main(String[] args) {
-        // I'll solve this problem after doing hashmap.
-    }
-}
