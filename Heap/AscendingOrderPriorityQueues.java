5a76e345f4e0970865c5f2dec8adf798ea9007ff
Manash <manashroy454@gmail.com>
2023-03-15 12:11:07 +0530

    Added Heap Folder

---
+++ Heap/AscendingOrderPriorityQueues.java
@@ -0,0 +1,18 @@
+import java.util.PriorityQueue;
+
+public class AscendingOrderPriorityQueues {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>();// Ascending Order Priority Queue
+        pq.add(3);// O(logn) Time complexity
+        pq.add(2);
+        pq.add(5);
+        pq.add(1);
+        pq.add(4);
+
+        while(!pq.isEmpty()){
+            System.out.println(pq.peek());// O(1) time cimplexity
+            pq.remove();
+        }
+
+    }
+}
---
+++ Heap/DescendingOrderPriorityQueues.java
@@ -0,0 +1,19 @@
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class DescendingOrderPriorityQueues {
+    public static void main(String[] args) {
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder()); // Descending Order Priority Queue
+        // Comparater is interface in JAVA
+        pq.add(3);// O(logn) Time complexity
+        pq.add(2);
+        pq.add(5);
+        pq.add(1);
+        pq.add(4);
+
+        while(!pq.isEmpty()){
+            System.out.println(pq.peek());// O(1) time cimplexity
+            pq.remove();// O(logn) Time complexity
+        }
+    }
+}
