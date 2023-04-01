3987d4d988c9d18b1e195661a816a7ea1c217e33
Manash <manashroy454@gmail.com>
2023-04-01 09:26:18 +0530

    Added file

---
+++ 16) Greedy/_01_ActivitySelection.java
@@ -0,0 +1,6 @@
+public class _01_ActivitySelection {
+    public static void main(String[] args) {
+        int[] start = { 1, 3, 0, 5, 8, 5 };
+        int[] end = { 2, 4, 6, 7, 9, 9 }; // End time is in sorted order
+    }
+}
