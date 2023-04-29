1a26724be693e721e9236e66301d929d0ed22dc4
Manash <manashroy454@gmail.com>
2023-04-29 18:52:16 +0530

    Added file

---
+++ 15) Greedy/_06_MinimumCoin.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class _06_MinimumCoin {
+
+    public static ArrayList<Integer> findMinCoins(int rupees, int[] coins) {
+        ArrayList<Integer> coinList = new ArrayList<>();
+        for (int i = coins.length - 1; i >= 0; i--) {
+            while (rupees >= coins[i]) {
+                rupees -= coins[i];
+                coinList.add(coins[i]);
+            }
+        }
+        return coinList;
+    }
+
+    public static void main(String[] args) {
+        int[] coins = { 1, 2, 5, 10, 20, 50, 100, 500, 1000 };
+        int rupees = 49;// Rupees is given. I have to find how can I can represent this amount of Rupees
+        // with this given coins. I have to take minimum number of coins from the Array
+        // 49 = 20+20+5+2+2 (So this much coin is required)
+        System.out.println(findMinCoins(rupees, coins));// [20, 20, 5, 2, 2]
+    }
+}
