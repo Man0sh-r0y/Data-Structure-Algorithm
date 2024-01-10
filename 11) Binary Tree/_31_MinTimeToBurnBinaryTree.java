d6d788d061ed1bef5cad8afc05059e44d322d5d0
Manash Roy <manashroy454@gmail.com>
2024-01-10 22:43:37 +0530

    updated the code

--- 11) Binary Tree/_31_MinTimeToBurnBinaryTree.java
+++ 11) Binary Tree/_31_MinTimeToBurnBinaryTree.java
@@ -1,5 +1,5 @@
-import java.util.LinkedList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Queue;
 
 public class _31_MinTimeToBurnBinaryTree {
@@ -16,39 +16,19 @@ public class _31_MinTimeToBurnBinaryTree {
         }
     }
 
-    // our required data structures to solve this problem
-    public static HashMap<Node, Node> trackParents = new HashMap<>();// it'll be storing parents of each nodes
-    public static HashMap<Node, Boolean> visitedNode = new HashMap<>();// It'll track if node is visited or not
-
-    public static void markParent(Node root) {
+    public static int getMinTime(Node root, Node startNode, HashMap<Node, Node> trackParents) {
         Queue<Node> q = new LinkedList<>();
-        q.add(root);
-        while (!q.isEmpty()) {
-            Node currNode = q.remove();
-            if (currNode.left != null) {
-                trackParents.put(currNode.left, currNode);// key: left child, value: currentNode
-                q.add(currNode.left);
-            }
-            if (currNode.right != null) {
-                trackParents.put(currNode.right, currNode);// key: right child, value: currentNode
-                q.add(currNode.right);
-            }
-        }
-    }
-
-    public static int getMinTime(Node root, Node targetNode) {
-        markParent(root);
-        Queue<Node> q = new LinkedList<>();
-        q.add(targetNode);
-        visitedNode.put(targetNode, true);
-        int time = 0;
+        HashMap<Node, Boolean> visitedNode = new HashMap<>();// It'll track if node is visited or not
+        q.add(startNode); // adding the starting node
+        visitedNode.put(startNode, true); // marking the starting node as visited
+        int time = 0; // it'll be the time to burn the whole tree
 
         while (!q.isEmpty()) {
 
             int size = q.size();
-            Boolean isChecked = false;// if Nodes are checked then it'll be false
+            Boolean isChecked = false;// if Nodes aren't checked then it is false
 
-            for (int i = 0; i < size; i++) {
+            for (int i = 0; i < size; i++) { // all the adjacent nodes will be burnt in one time
                 Node currNode = q.remove();
                 // left checking
                 if (currNode.left != null && visitedNode.get(currNode.left) == null) {
@@ -71,13 +51,40 @@ public class _31_MinTimeToBurnBinaryTree {
                     q.add(parentNode);
                 }
             }
+
             // incrementing time
-            if (isChecked) {
-                time++;
-            }
+            if (isChecked) time++;
         }
 
         return time;
+    }
+
+    public static Node markParent(Node root, int start, HashMap<Node, Node> trackParents) {
+        Queue<Node> q = new LinkedList<>();
+        Node startNode = new Node(-1); // it'll be the starting node from where the fire starts
+        q.add(root); // adding root node
+
+        while (!q.isEmpty()) {
+            Node currNode = q.remove();
+            if(currNode.data == start) 
+                startNode = currNode; // mark the starting node
+            if (currNode.left != null) {
+                trackParents.put(currNode.left, currNode);// key: left child, value: currentNode
+                q.add(currNode.left);
+            }
+            if (currNode.right != null) {
+                trackParents.put(currNode.right, currNode);// key: right child, value: currentNode
+                q.add(currNode.right);
+            }
+        }
+        return startNode; // returning the starting node from where the fire starts
+    }
+
+    public static int timeToBurnBinaryTree(Node root, int start){
+        HashMap<Node, Node> trackParents = new HashMap<>();// it'll be storing parents of each nodes
+        Node startNode = markParent(root, start, trackParents); // startNode defines the node from where the fire starts
+        int minTimeToBurn = getMinTime(root, startNode, trackParents); // minimum time to burn the whole tree
+        return minTimeToBurn;
     }
 
     public static void main(String[] args) {
@@ -89,6 +96,8 @@ public class _31_MinTimeToBurnBinaryTree {
         root.right.left = new Node(5);
         root.right.right = new Node(6);
 
-        System.out.println(getMinTime(root, root.left));
+        //System.out.println(getMinTime(root, root.left));
+
+        System.out.println(timeToBurnBinaryTree(root, 2)); // sending the root node and the starting node from where the fire starts
     }
 }
