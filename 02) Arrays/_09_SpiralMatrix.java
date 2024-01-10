5d0105ab1e77a58617eb3a441530b94d6182da21
Manash Roy <manashroy454@gmail.com>
2024-01-11 01:30:04 +0530

    updated the code

--- 02) Arrays/_09_SpiralMatrix.java
+++ 02) Arrays/_09_SpiralMatrix.java
@@ -1,4 +1,21 @@
+
 public class _09_SpiralMatrix {
+
+    // LOGIC:
+    // The goal is to print the elements of a matrix in a spiral order, starting from the top-left corner and moving in a clockwise direction.
+    // Take variables (startRow, startCol, endRow, endCol) to represent the current boundaries of the matrix to be printed.
+    // Iterate through the matrix in a spiral order until all elements are printed.
+    // For each side of the spiral (top, right, bottom, left), use separate loops to print the elements:
+    // Print the top side from startCol to endCol.
+    // Print the right side from startRow + 1 to endRow.
+    // Print the bottom side from endCol - 1 to startCol (if there is more than one row).
+    // Print the left side from endRow - 1 to startRow + 1 (if there is more than one column).
+    // Ensure that the loops for printing the bottom and left sides check for the condition startRow != endRow or startCol != endCol 
+    // So that it can avoid redundant printing in case there is only one row or one column remaining.
+    // this senario can be happended when you have a matrix with dimensions 1xN (one row and multiple columns) or Nx1 (one column and multiple rows).
+    // Without the check for redundancy, the algorithm might print the elements twice: 
+    // once when printing the top side, and again when printing the bottom side (or similarly, for left and right sides).
+    // continue the while loop until startRow <= endRow and startCol <= endCol.
 
     public static void printSpiralMatrix(int matrix[][]) {
         int startRow = 0, startCol = 0;
