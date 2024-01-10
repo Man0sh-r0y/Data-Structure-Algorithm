aede767f4cef3dc10ccfd74db451088dc5521bc1
Manash Roy <manashroy454@gmail.com>
2024-01-10 12:58:22 +0530

    Updated the code

--- 02) Arrays/_04_pairsInArray.java
+++ 02) Arrays/_04_pairsInArray.java
@@ -2,14 +2,6 @@ import java.util.*;
 
 public class _04_pairsInArray {
 
-    public static void arrayInput(int arr[]) { // Array Element input
-        Scanner sc = new Scanner(System.in);
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print("Enter the Element " + (i + 1) + ": ");
-            arr[i] = sc.nextInt();
-        }
-    }
-
     public static void pairsInArray(int arr[]) {
         for (int i = 0; i < arr.length; i++) {
             for (int j = i + 1; j < arr.length; j++) {
@@ -24,9 +16,15 @@ public class _04_pairsInArray {
         System.out.print("Enter the size of array: ");
         int n = sc.nextInt();
         int arr[] = new int[n];
+
         // Array Element input
-        arrayInput(arr);
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("Enter the Element " + (i + 1) + ": ");
+            arr[i] = sc.nextInt();
+        }
+        
         // Pairs in Array
         pairsInArray(arr);
+        sc.close();
     }
 }
