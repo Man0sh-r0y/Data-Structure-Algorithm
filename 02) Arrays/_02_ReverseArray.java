622ce81c4479e50f6b31cc3afe79e2064c1e9ae0
Manash Roy <manashroy454@gmail.com>
2024-01-10 09:53:51 +0530

    updated file

--- 02) Arrays/_02_ReverseArray.java
+++ 02) Arrays/_02_ReverseArray.java
@@ -2,12 +2,19 @@ import java.util.*;
 
 public class _02_ReverseArray {
 
+    // LOGIC
+    // 1. Run the loop from i=0 to i=n/2 in the array
+    // 2. Swap the ith element with (n-1-i)th element 
+    // 3. That means, swap the first element with the last element, second element with second last element and so on
+    // 4. Time Complexity: O(n)
+
     public static void arrayInput(int arr[]) {
         Scanner sc = new Scanner(System.in);
         for (int i = 0; i < arr.length; i++) {
             System.out.print("Enter the Element " + (i + 1) + ": ");
             arr[i] = sc.nextInt();
         }
+        sc.close();
     }
 
     public static void ReverseArray(int arr[]) {
@@ -34,5 +41,6 @@ public class _02_ReverseArray {
         arrayInput(arr);
         ReverseArray(arr);
         printArray(arr);
+        sc.close();
     }
 }
