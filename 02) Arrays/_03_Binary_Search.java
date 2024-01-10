2719af1c0c2376c45339408e7b9785e43d2d0f55
Manash Roy <manashroy454@gmail.com>
2024-01-10 12:32:48 +0530

    added notes

--- 02) Arrays/_03_Binary_Search.java
+++ 02) Arrays/_03_Binary_Search.java
@@ -2,6 +2,17 @@ import java.util.*;
 
 public class _03_Binary_Search {
 
+    // LOGIC
+    // 1. Sort the array
+    // 2. while sorting check if the array is already sorted
+    // 3. Find the middle element of the array
+    // 4. Compare the middle element with the search element
+    // 5. If the search element is less than the middle element then search in the left half
+    // 6. If the search element is greater than the middle element then search in the right half
+    // 7. If the search element is equal to the middle element then return the index of the middle element
+    // 8. If the search element is not found then return -1
+    // 9. Repeat the steps 3 to 8 until the element is found or the start is less than or equal to the end
+
     public static void bubbleSort(int arr[]) {
         boolean preSorted = true;
         for (int i = 0; i < arr.length - 1; i++) {
