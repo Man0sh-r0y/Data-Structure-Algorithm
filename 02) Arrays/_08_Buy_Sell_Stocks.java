34df442a8a11112eecaf4230e36fb3d66b02057a
Manash Roy <manashroy454@gmail.com>
2024-01-11 00:54:59 +0530

    added the notes

--- 02) Arrays/_08_Buy_Sell_Stocks.java
+++ 02) Arrays/_08_Buy_Sell_Stocks.java
@@ -1,15 +1,16 @@
 public class _08_Buy_Sell_Stocks {
 
+    // LOGIC:
+    // The goal is to find the maximum profit that can be obtained by buying and selling a stock given its daily prices.
     // I will buy the stock when price is low
     // I will sell the stock when price is high
     // So I can earn max profit
     // I will be given the price array
     // at the 1st day, I have to buy the stock as this is the 1st day
-    // after that day, I will comapare the previous buying price with the current price
-    // if the current price is less than the previous buying price, I will buy the stock
-    // if the current price is greater than the previous buying price, I will sell the stock
-    // that's how I will evaluate the max profit from the given price array
-    // With this algo, I can tell when max profit can be generated
+    // For each day, compare the current stock price with the buyPrice:
+    // If the current stock price is lower than the buyPrice, update the buyPrice to the current day's price, representing a potential buying opportunity.
+    // If the current stock price is higher than the buyPrice, calculate the profit that could be obtained by selling at the current price (price[i] - buyPrice).
+    // Update the maxProfit if the calculated profit is greater than the current maximum profit.
 
     public static int buy_sell_Stocks(int price[]) {
         int buyPrice = Integer.MAX_VALUE, maxProfit = 0;
