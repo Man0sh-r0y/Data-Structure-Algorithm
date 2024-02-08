739e89507e5873dc4f29cedaecb85f83f5d4c61b
Manash Roy <manashroy454@gmail.com>
2024-02-09 01:03:04 +0530

    updated the file

--- 02) Arrays/_16_SecondLargestNumber.java
+++ 02) Arrays/_16_SecondLargestNumber.java
@@ -1,6 +1,14 @@
 import java.util.Arrays;
 class _16_SecondLargestNumber {
 
+    // SECOND LARGEST ELEMENT
+    // 1. If current element is greater than largest (As We took a largest variable), then update largest 
+    // 2. if current element isn't greater than largest, then check if it is greater than second largest then update second largest
+
+    // SECOND SMALLEST ELEMENT
+    // 1. If current element is smaller than smallest(As We took a smallest variable), then update smallest
+    // 2. If current element isn't smaller than smallest, then check if it is smaller than second smallest then update second smallest
+
     public static int[] getSecondOrderElements(int n, int []arr) {// O(n)
  
         int largest=Integer.MIN_VALUE, secndLargest=Integer.MIN_VALUE;
