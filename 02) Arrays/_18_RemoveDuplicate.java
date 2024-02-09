9297597b0eb8e5bdb3ee03ec0f9b975ac0bc0a53
Manash Roy <manashroy454@gmail.com>
2024-02-09 21:02:09 +0530

    updated the file

--- 02) Arrays/_18_RemoveDuplicate.java
+++ 02) Arrays/_18_RemoveDuplicate.java
@@ -1,5 +1,24 @@
+import java.util.HashMap;
+
 class _18_RemoveDuplicate{
-  puplic static int removeDuplicate(int n, int[] arr){
-    
+  public static int removeDuplicates(int[] arr,int n) {
+    if(arr.length == 1) return 1;
+
+    HashMap <Integer, Integer> map = new HashMap<>();
+
+    for(int i=0; i<n; i++) {
+      if(map.containsKey(arr[i])) {
+        map.put(arr[i], map.get(arr[i])+1);
+      } else {
+        map.put(arr[i], 1);
+      }
+    }
+
+    return map.size();
+  }
+
+  public static void main(String[] args) {
+    int[] arr = new int[]{1,2,2,2,3};
+    System.out.println(removeDuplicates(arr, arr.length));
   }
 }
