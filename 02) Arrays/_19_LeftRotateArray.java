e18ce5a14c4245acc6f4574f98e09da7cbc46599
Man0sh-r0y <96605313+Man0sh-r0y@users.noreply.github.com>
2024-02-10 16:44:34 +0000

    updated the file

---
+++ 02) Arrays/_19_LeftRotateArray.java
@@ -0,0 +1,28 @@
+import java.util.* ;
+import java.io.*; 
+
+
+public class _19_LeftRotateArray {
+
+    public static int[] rotateArray(int[] arr, int n) {
+      if(arr.length == 1) return arr;
+
+      int firstElement = arr[0];
+
+      for(int i=0; i<n; i++){
+        if(i==n-1){
+          arr[i]=firstElement;
+        } else {
+          arr[i]=arr[i+1];
+        }
+      }
+
+      return arr;
+
+    }
+
+  public static void main(String[] args) {
+    int[] arr = new int[]{1,2,3,4,5};
+    System.out.println(Arrays.toString(rotateArray(arr, arr.length)));
+  }
+}
