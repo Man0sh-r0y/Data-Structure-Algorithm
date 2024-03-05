af62016938f1522e224c90013c5fac6f78d74e96
Manash Roy <manashroy454@gmail.com>
2024-03-05 09:43:54 +0530

    updated the file

--- 02) Arrays/_30_MajorityElement.java
+++ 02) Arrays/_30_MajorityElement.java
@@ -23,6 +23,38 @@ public class _30_MajorityElement {
         return -1;
     }
 
+    // Approach 2: Moore's Voting Algorithm
+    public static int majorityElement2(int[] nums) {
+        int element = nums[0], count = 1; // element is initialized to nums[0] and count is incremented by 1
+
+        // find the expected majority element
+        for(int i=1; i<nums.length; i++) {
+            if(count == 0){
+                element = nums[i];
+                count++;
+            }
+            else if(nums[i] == element) {
+                count++;
+            }
+            else {
+                count--;
+            }
+        }
+
+        // if majority element is guaranteed to exist then the above loop is enough to reach the majority element, no needed to apply the below loop 
+
+        int freq = 0; // count the frequency of the expected majority element
+
+        // check if the expected majority element is the majority element
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] == element) {
+                freq++;
+            }
+        }
+
+        return (freq > nums.length / 2) ? element : -1; // return the majority element if it exists, else return -1
+    }
+
     public static void main(String[] args) {
         int[] arr = new int[]{3,2,3};
         System.out.println(majorityElement(arr));
