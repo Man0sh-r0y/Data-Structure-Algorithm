1e5739ca5b5daaca4635e73411cfde4fcd80bedd
Manash Roy <manashroy454@gmail.com>
2024-07-16 12:29:24 +0530

    updated the file

--- 15) Greedy/_11_AssignCookies.java
+++ 15) Greedy/_11_AssignCookies.java
@@ -3,15 +3,21 @@ import java.util.*;
 public class _11_AssignCookies {
 
     public static int findContentChildren(int[] g, int[] s) {
-        Arrays.sort(g);
-        Arrays.sort(s);
+        Arrays.sort(g); // g[] determines the greed factor of each child
+        Arrays.sort(s); // s[] determines the size of each cookie
 
-        int i = 0; // each child is denoted by i
-        int  j = 0; // each cookie is denoted by j
+        int i = 0; // it will traverse through g[]
+        int  j = 0; // it will traverse through s[]
         int child = 0; // number of children who can eat the cookie
 
         while(i < g.length && j < s.length) {
-            
+            if(s[j] >= g[i]) {
+                child++; // number of children who can eat the cookie
+                i++; // increement
+                j++; // increment
+            } else {
+                j++; // only increment j index so that I can get bigger size of cookie
+            }
         }
 
         return child;
