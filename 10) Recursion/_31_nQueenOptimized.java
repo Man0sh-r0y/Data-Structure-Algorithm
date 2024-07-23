d6460ed5eb2ad3fdf2ccde61eb697583a5c69def
Manash Roy <manashroy454@gmail.com>
2024-07-24 00:52:03 +0530

    updated

--- 10) Recursion/_31_nQueenOptimized.java
+++ 10) Recursion/_31_nQueenOptimized.java
@@ -1,14 +1,21 @@
+import java.util.*;
+
 public class _31_nQueenOptimized {
 
-    public static int n = 4;
-
-    public static boolean leftRow[] = new boolean[n];
-    public static boolean upperDiagonal[] = new boolean[2 * n - 1];
-    public static boolean lowerDiagonal[] = new boolean[2 * n - 1];
-
-    public static void insertQueen(char board[][], int col) {
+    public static void insertQueen(char board[][], int col, int n, boolean leftRow[], boolean upperDiagonal[], boolean lowerDiagonal[], List<List<String>> ans) {
         if (col == n) {
-            printQueen(board);
+            printQueen(board, n);
+
+            // print the output in given format (According to LeetCode)
+            List<String> temp = new ArrayList<>();
+
+            for(int i=0; i<board.length; i++) {
+                String s = new String(board[i]);
+                temp.add(s);
+            }
+
+            ans.add(temp);
+
             return;
         }
 
@@ -19,9 +26,9 @@ public class _31_nQueenOptimized {
                 lowerDiagonal[col + row] = true;
 
                 board[row][col] = 'Q';
-                insertQueen(board, col + 1);
+                insertQueen(board, col + 1, n, leftRow, upperDiagonal, lowerDiagonal, ans);
                 // backtrack
-                board[row][col] = 'X';
+                board[row][col] = '.';
 
                 leftRow[row] = false;
                 upperDiagonal[n - 1 + col - row] = false;
@@ -30,7 +37,7 @@ public class _31_nQueenOptimized {
         }
     }
 
-    public static void printQueen(char board[][]) {
+    public static void printQueen(char board[][], int n) {
         System.out.println("---------------");
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
@@ -41,12 +48,23 @@ public class _31_nQueenOptimized {
     }
 
     public static void main(String[] args) {
+        int n = 4;
+
         char board[][] = new char[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n; j++) {
-                board[i][j] = 'X';
+                board[i][j] = '.';
             }
         }
-        insertQueen(board, 0);
+
+        boolean leftRow[] = new boolean[n];
+        boolean upperDiagonal[] = new boolean[2 * n - 1];
+        boolean lowerDiagonal[] = new boolean[2 * n - 1];
+
+        List<List<String>> ans = new ArrayList<>();
+
+        insertQueen(board, 0, n, leftRow, upperDiagonal, lowerDiagonal, ans);
+
+        System.out.println(ans);
     }
 }
