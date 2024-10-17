f270c2f421ca1cd4f3c7a0e6264b97762dbbc297
Manash Roy <manash.roy.dev@gmail.com>
2024-10-17 22:47:01 +0530

    updated the file

--- 08) Stack/_17_NextGreaterElementI.java
+++ 08) Stack/_17_NextGreaterElementI.java
@@ -3,23 +3,34 @@ import java.util.HashMap;
 
 public class _17_NextGreaterElementI {
 
-    static class Pair {
-        int index;
-        int nextGreater;
-
-        Pair(int index, int nextGreater) {
-            this.index = index;
-            this.nextGreater = nextGreater;
-        }
-    }
-
-    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {
+    public static int[] nextGreaterElement(int[] nums1, int[] nums2) {// O(M + N)
         int[] ans = new int[nums1.length];
 
         HashMap<Integer, Integer> map = new HashMap<>();
 
         for(int i=nums2.length-1; i>=0; i--) {
-            
+            map.put(nums2[i], i);
+        }
+
+        System.out.println(map);
+
+        for(int i=0; i<nums1.length; i++) {
+            int max = nums1[i];
+
+            if(map.containsKey(nums1[i])) {
+
+                for(int j=map.get(nums1[i]); j<nums2.length; j++) {
+
+                    if(nums2[j] > max) {
+                        ans[i] = nums2[j];
+                        max = nums2[j];
+                        break;
+                    }
+                }
+
+                if(max == nums1[i]) 
+                    ans[i] = -1;
+            }
         }
 
         return ans;
